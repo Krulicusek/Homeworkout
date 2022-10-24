5f67850eac0a404d11d0da400bd6d6408d3569b9
unknown <kubawegrzynowski@gmail.com>
2022-10-24 21:19:49 +0200

    moved backend to HomeWorkoutBackend

---
+++ HomeWorkoutBackend/HomeWorkoutBackend/Controllers/ExerciseModelsController.cs
@@ -0,0 +1,22 @@
+﻿using homeWorkOutApi.Net6._0.Data;
+using HomeWorkOutApi.NetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWorkoutBackend.Controllers
+{
+    public class ExerciseModelsController : Controller
+    {
+        private readonly homeWorkOutApiNet6_0Context _context;
+
+        public ExerciseModelsController(homeWorkOutApiNet6_0Context context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<ExerciseModel> Get()
+        {
+            return _context.ExerciseModel.ToArray();
+        }
+
+    }
+}
---
+++ HomeWorkoutBackend/HomeWorkoutBackend/Program.cs
@@ -0,0 +1,48 @@
+using homeWorkOutApi.Net6._0.Data;
+using homeWorkOutApi.Net6.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+IConfiguration configuration = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile("appsettings.json");
+});
+
+AppSettings appSettings = new AppSettings(configuration);
+appSettings.GetServerSettings();
+
+if (!string.IsNullOrEmpty(appSettings?.ServerSettings?.ConnectionString))
+{
+    builder.Services.AddDbContext<homeWorkOutApiNet6_0Context>(options =>
+    options.UseSqlServer(appSettings.ServerSettings.ConnectionString));
+}
+builder.Services.AddDbContext<homeWorkOutApiNet6_0Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("homeWorkOutApiNet6_0Context")));
