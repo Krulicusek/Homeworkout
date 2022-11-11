0ba64c3720082596533bc3d3fb56122096a1a513
unknown <kubawegrzynowski@gmail.com>
2022-11-11 17:48:35 +0100

    .net 7, Working on ExerciseService

---
+++ HomeWorkoutFrontend/SharedUILibrary/Services/ExerciseService.cs
@@ -0,0 +1,21 @@
+﻿using HomeWorkoutModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedUILibrary.Services
+{
+    public class ExerciseService
+    {
+        private readonly HttpClient httpClient;
+        public List<ExerciseModel> GetExerciseModels()
+        {
+            List<ExerciseModel> exerciseModels= new List<ExerciseModel>();
+            exerciseModels = httpClient.GetFromJsonAsync<List<ExerciseModel>>("api/ExerciseModel").Result;
+            return exerciseModels;
+        }
+    }
+}
