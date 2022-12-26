9ff9d0bab7a14de420ead3c13edca6da91bf761a
unknown <kubawegrzynowski@gmail.com>
2022-12-26 01:51:12 +0100

    added PatientController and Patients.razor

---
+++ HomeWorkoutBackend/HomeWorkoutBackend/Controllers/PatientController.cs
@@ -0,0 +1,28 @@
+﻿using homeWorkOutApi.Net6._0.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWorkoutBackend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly HomeWorkoutContext _context;
+        private readonly ILogger<HomeworkSequenceController> _logger;
+        public PatientController(HomeWorkoutContext context, ILogger<HomeworkSequenceController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        [HttpGet("{id}", Name = "GetPatients")]
+        public List<User> GetPatients(int id)
+        {
+            return _context.Users.Where(x=>x.PhysioId == id).ToList();
+        }
+        [HttpGet(Name = "GetPhysiotherapists")]
+        public List<User> GetPhysiotherapists()
+        {
+            return _context.Users.Where(x => x.Role == "physiotherapist").ToList();
+        }
+    }
+}
