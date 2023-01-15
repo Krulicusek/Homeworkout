dc9fce94a7ba843b135aa43ed2701d83922c76f4
Mariusz Miłoń <marimil224@student.polsl.pl>
2023-01-15 11:45:49 +0100

    Added psyhio pages

--- HomeWorkoutBackend/HomeWorkoutBackend/Controllers/PatientController.cs
+++ HomeWorkoutBackend/HomeWorkoutBackend/Controllers/PatientController.cs
@@ -1,6 +1,7 @@
 ﻿using homeWorkOutApi.Net6._0.Data;
 using HomeWorkoutModels.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HomeWorkoutBackend.Controllers
 {
@@ -40,6 +41,26 @@ namespace HomeWorkoutBackend.Controllers
                 basicDetails.Add(userBasic);
             }
             return basicDetails;
+        }
+
+        [HttpPut]
+        [Route("SetPhysiotherapists")]
+        public async Task SetPhysiotherapists(int userId, int psychoId)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+            user.PhysioId = psychoId;
+            _context.Update(user);
+            _context.SaveChanges();
+        }
+
+        [HttpGet]
+        [Route("AssignedPsycho")]
+        public IActionResult AssignedPsycho(int userId)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == userId);
+            if (user.PhysioId == null)
+                return NotFound();
+            return Ok();
         }
     }
 }
--- HomeWorkoutFrontend/SharedUILibrary/Services/PatientService.cs
+++ HomeWorkoutFrontend/SharedUILibrary/Services/PatientService.cs
@@ -15,6 +15,25 @@ namespace SharedUILibrary.Services
         {
             this.httpClient = httpClient;
         }
+
+        public async Task ChoosePhysiotherapist(string userId, string psychoId)
+        {
+            var userIdInt = int.Parse(userId);
+            var psychoIdInt = int.Parse(psychoId);
+            await httpClient.PutAsync($"/Patient/SetPhysiotherapists?userId={userIdInt}&psychoId={psychoIdInt}", null);
+        }
+
+        public async Task<bool> AssignedPsycho(int id)
+        {
+            var result = await httpClient.GetAsync($"/Patient/AssignedPsycho?userId={id}");
+            if(result.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
         public List<UserBasicDetail> GetPhysiotherapists()
         {
            return httpClient.GetFromJsonAsync<List<UserBasicDetail>>($"/Patient/GetPhysiotherapists").Result;
